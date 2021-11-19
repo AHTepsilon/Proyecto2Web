1cb6a1ec74cb2c0b0055a0be38ed478f118d8b3d
AHTepsilon <alehertoro725@gmail.com>
2021-11-19 17:50:18 -0500

    added initial code for sign up and login screens

---
+++ src/loginScreen.js
@@ -0,0 +1,13 @@
+import {initializeApp} from "firebase/app";
+import {getDatabase, ref, set, onValue, push} from "firebase/database";
+
+import {getFirebaseConfig} from "./firebase-config";
+
+const firebaseAppConfig = getFirebaseConfig();
+const firebaseApp = initializeApp(firebaseAppConfig);
+
+const username = document.getElementById("usernameText");
+const password = document.getElementById("passwordText");
+
+const logBtn = document.getElementById("logButton");
+const forgotPassBtn = document.getElementById("forgotPassButton");
---
+++ src/signUpScreen.js
@@ -0,0 +1,14 @@
+import {initializeApp} from "firebase/app";
+import {getDatabase, ref, set, onValue, push} from "firebase/database";
+
+import {getFirebaseConfig} from "./firebase-config";
+
+const firebaseAppConfig = getFirebaseConfig();
+const firebaseApp = initializeApp(firebaseAppConfig);
+
+const username = document.getElementById("usernameText");
+const email = document.getElementById("emailText");
+const password = document.getElementById("passwordText");
+const cPassword = document.getElementById("cPasswordText");
+
+const sUpBtn = document.getElementById("logButton");
