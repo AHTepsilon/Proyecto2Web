0839ff37865948dc46862ea5ccd1693cf364b6d6
AHTepsilon <alehertoro725@gmail.com>
2021-11-26 08:04:57 -0500

    added sign up system

--- src/signUpScreen.js
+++ src/signUpScreen.js
@@ -2,9 +2,12 @@ import {initializeApp} from "firebase/app";
 import {getDatabase, ref, set, onValue, push} from "firebase/database";
 
 import {getFirebaseConfig} from "./firebase-config";
+import {getAuth, createUserWithEmailAndPassword} from 'firebase/auth';
 
 const firebaseAppConfig = getFirebaseConfig();
 const firebaseApp = initializeApp(firebaseAppConfig);
+const db = getDatabase();
+const auth = getAuth();
 
 const username = document.getElementById("usernameText");
 const email = document.getElementById("emailText");
@@ -13,7 +16,27 @@ const cPassword = document.getElementById("cPasswordText");
 
 const sUpBtn = document.getElementById("logButton");
 
-sUpBtn.addEventListener("click", function()
+function switchScreen()
 {
     window.location.href = "landingPage.html";
+}
+
+function register(e, event)
+{
+    createUserWithEmailAndPassword(auth, email.value, password.value)
+    .then((user_credential) => 
+    {
+        console.log(user_credential);
+        switchScreen();
+    })
+    .catch((error) =>
+    {
+        alert("Email or password invalid, please try again");
+        console.log(error.message);
+    });
+}
+
+sUpBtn.addEventListener("click", function()
+{
+    register();
 });
