001ab2d454ba847cfc0335a43cf590f6c8839d3b
AHTepsilon <alehertoro725@gmail.com>
2021-11-29 16:29:49 -0500

    implemented login

--- src/loginScreen.js
+++ src/loginScreen.js
@@ -2,17 +2,42 @@ import {initializeApp} from "firebase/app";
 import {getDatabase, ref, set, onValue, push} from "firebase/database";
 
 import {getFirebaseConfig} from "./firebase-config";
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth';
 
 const firebaseAppConfig = getFirebaseConfig();
 const firebaseApp = initializeApp(firebaseAppConfig);
 
-const username = document.getElementById("usernameText");
+const db = getDatabase();
+const auth = getAuth();
+
+const email = document.getElementById("emailText");
 const password = document.getElementById("passwordText");
 
 const logBtn = document.getElementById("logButton");
 const forgotPassBtn = document.getElementById("forgotPassButton");
 
-logBtn.addEventListener("click", function()
+function login()
+{
+    signInWithEmailAndPassword(auth, email.value, password.value).then(
+        (user_credential) =>
+        {
+            switchScreen();
+        }
+    )
+    .catch(
+        (error) =>
+        {
+            alert("Usuario o contraseña incorrectos");
+        }
+    )
+}
+
+function switchScreen()
 {
     window.location.href = "landingPage.html";
+}
+
+logBtn.addEventListener("click", function()
+{
+    login();
 });
