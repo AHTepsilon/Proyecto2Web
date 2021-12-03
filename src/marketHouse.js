a83e749f93ba44defa743fa108b14bb819de6135
AHTepsilon <alehertoro725@gmail.com>
2021-12-03 15:20:22 -0500

    added database and cards for food at home

--- src/marketHouse.js
+++ src/marketHouse.js
@@ -2,9 +2,11 @@ import {initializeApp} from "firebase/app";
 import {getDatabase, ref, set, onValue, push} from "firebase/database";
 
 import {getFirebaseConfig} from "./firebase-config";
+import { marketHouse_foodCards } from "./marketHouse_foodCards";
 
 const firebaseAppConfig = getFirebaseConfig();
 const firebaseApp = initializeApp(firebaseAppConfig);
+const db = getDatabase();
 
 const scannedItemsBtn = document.getElementById("scannedItemsButton");
 const marketHouseBtn = document.getElementById("marketHouseButton");
@@ -12,7 +14,19 @@ const savedBtn = document.getElementById("savedButton");
 const userBtn = document.getElementById("userButton");
 const calendarBtn = document.getElementById("calendar");
 
+const fruitsBtn = document.getElementById("fruitsButton");
+const vegsBtn = document.getElementById("vegsButton");
+const nutsBtn = document.getElementById("nutsButton");
+
 const logoHyperlink = document.getElementById("imageLogo");
+
+const foodCardsD = document.getElementById("foodCardsDiv");
+
+const orangeImg = document.getElementById("orangeSq");
+const yellowImg = document.getElementById("yellowSq");
+const blueImg = document.getElementById("blueSq");
+const greenImg = document.getElementById("greenSq");
+
 
 logoHyperlink.addEventListener("click", function()
 {
@@ -34,3 +48,106 @@ savedBtn.addEventListener("click", function()
 {
     window.location.href = "statistics.html";
 });
+
+function getFruits()
+{
+    const dbRef = ref(db, 'foodAtHome' + '/' + 'fruits');
+
+    onValue(dbRef, (snapshot) =>
+    {
+        const data = snapshot.val();
+        console.log(data);
+        updateFoodList(data);
+    });
+}
+
+function getVegs()
+{
+    const dbRef = ref(db, 'foodAtHome' + '/' + 'vegetables');
+
+    onValue(dbRef, (snapshot) =>
+    {
+        const data = snapshot.val();
+        console.log(data);
+        updateFoodList(data);
+    });
+}
+
+function getNuts()
+{
+    const dbRef = ref(db, 'foodAtHome' + '/' + 'nuts');
+
+    onValue(dbRef, (snapshot) =>
+    {
+        const data = snapshot.val();
+        console.log(data);
+        updateFoodList(data);
+    });
+}
+
+function updateFoodList(info)
+{
+    if(info)
+    {
+        foodCardsD.innerHTML = "";
+
+        Object.keys(info).forEach((key, index) =>
+        {
+            const foodItem = new marketHouse_foodCards(info[key]);
+            foodCardsD.appendChild(foodItem.render());
+
+            console.log(info[key].EXPIRES);
+
+            if(info[key].EXPIRES >= 15)
+            {
+                const aye = document.createElement("img");
+                aye.src = "images/gren.png";
+                aye.className = "ayeImg";
+
+                foodCardsD.appendChild(aye);
+            }
+    
+            else if(info[key].EXPIRES >= 5 && info[key].EXPIRES < 15)
+            {
+                const aye = document.createElement("img");
+                aye.src = "images/blu.png";
+                aye.className = "ayeImg";
+
+                foodCardsD.appendChild(aye);
+            }
+    
+            else if(info[key].EXPIRES >= 1 && info[key].EXPIRES < 5)
+            {
+                const aye = document.createElement("img");
+                aye.src = "images/yello.png";
+                aye.className = "ayeImg";
+
+                foodCardsD.appendChild(aye);
+            }
+            
+            else if(info[key].EXPIRES < 1)
+            {
+                const aye = document.createElement("img");
+                aye.src = "images/oreng.png";
+                aye.className = "ayeImg";
+
+                foodCardsD.appendChild(aye);
+            }
+        });
+    }
+}
+
+fruitsBtn.addEventListener("click", (e, ev)=>
+{
+    getFruits();
+});
+
+vegsBtn.addEventListener("click", (e, ev) =>
+{
+    getVegs();
+});
+
+nutsBtn.addEventListener("click", (e, ev) =>
+{
+    getNuts();
+})
---
+++ src/marketHouse_foodCards.js
@@ -0,0 +1,83 @@
+import { getDatabase, ref, set, onValue, push } from 'firebase/database';
+
+export class marketHouse_foodCards
+{
+    constructor(foodItem)
+    {
+        this.foodItem = foodItem;
+    }
+
+    render()
+    {
+        let card = document.createElement("div");
+        card.className = "food_card";
+
+        let foodName = document.createElement("h2");
+        foodName.className = "food_card_name";
+        foodName.innerHTML = this.foodItem.NAME;
+
+        let foodQuantity = document.createElement("h3");
+        foodQuantity.className = "food_card_amount";
+        foodQuantity.innerHTML = "Amount: " + this.foodItem.QUANTITY;
+
+        let foodWeight = document.createElement("h3");
+        foodWeight.className = "food_card_weight";
+        foodWeight.innerHTML = "Weight: " + this.foodItem.WEIGHT + "kg";
+
+        let foodBoughtIn = document.createElement("h4");
+        foodBoughtIn.className = "food_bought_days";
+        foodBoughtIn.innerHTML = "How long ago was it bought: " + this.foodItem.BOUGHT + " days ago";
+
+        let foodExpiresIn = document.createElement("h4");
+        foodExpiresIn.className = "food_expires_in";
+        foodExpiresIn.innerHTML = "Expires in: " + this.foodItem.EXPIRES + " days";
+
+        card.appendChild(foodName);
+        card.appendChild(foodQuantity);
+        card.appendChild(foodWeight);
+        card.appendChild(foodBoughtIn);
+        card.appendChild(foodExpiresIn);
+
+        return card;
+    }
+
+    /*freshnessColor()
+    {
+        document.addEventListener("DOMContentLoaded", function()
+        {
+            if(this.foodItem.EXPIRES >= 15)
+            {
+                //document.getElementById("food_card_name").style.backgroundColor = "purple";
+
+                console.log("1");
+            }
+    
+            else if(this.foodItem.EXPIRES >= 5 && this.foodItem.EXPIRES < 15)
+            {
+                //cardBg.style.backgroundColor = "#65DEF1";
+    
+                //document.getElementById("food_card_name").style.backgroundColor = "purple";
+
+                console.log("2");
+            }
+    
+            else if(this.foodItem.EXPIRES >= 1 && this.foodItem.EXPIRES < 5)
+            {
+                //cardBg.style.backgroundColor = "#F7B801";
+    
+               // document.getElementById("food_card_name").style.backgroundColor = "purple";
+
+               console.log("3");
+            }
+            
+            else if(this.foodItem.EXPIRES < 1)
+            {
+                //cardBg.style.backgroundColor = "#FF7A00";
+    
+                //document.getElementById("food_card_name").style.backgroundColor = "purple";
+
+                console.log("4");
+            }
+        })
+    }*/
+}
